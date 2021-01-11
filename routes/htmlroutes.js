bd8c6defc02fb3363789ca12293fe824ecd2fafa
Eric Ober <ericscottober@gmail.com>
2021-01-11 18:18:06 -0500

    Added html rotues js file containing dependecies, routing, and html get requests

---
+++ routes/htmlroutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+var path = require("path");
+
+// Routing
+module.exports = function(app) {
+  
+// HTML GET Requests
+app.get("/notes", function(req, res) {
+res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+};
